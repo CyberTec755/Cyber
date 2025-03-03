f25a879197bb16838b59993656342aae61d7b083
Cyber <teccyber471@gmail.com>
2025-03-03 16:29:14 -0300

    .

---
+++ JS/script.js
@@ -0,0 +1,41 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+let slideIndexAuto = 0;
+autoShowSlides();
+
+function autoShowSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndexAuto++;
+    if (slideIndexAuto > slides.length) { slideIndexAuto = 1 }
+    slides[slideIndexAuto - 1].style.display = "block";
+    setTimeout(autoShowSlides, 8000); 
+}
