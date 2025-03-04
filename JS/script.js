b959a8e03983342755e59f193008233af70521c3
Pedro_Daniel <pedrodaniel4878@gmail.com>
2025-03-03 23:37:44 -0300

    df

--- JS/script.js
+++ JS/script.js
@@ -36,3 +36,33 @@ showSlides(slideIndex);
 setInterval(function() {
     plusSlides(1);
 }, 5000);
+
+  // Função para carregar os itens do carrinho
+    function adicionarAoCarrinho() {
+    let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+    const carrinhoContainer = document.getElementById('carrinho-container');
+    const totalContainer = document.getElementById('total-container');
+    let total = 0;
+
+    carrinhoContainer.innerHTML = 'X'; // Limpa os itens existentes
+
+    if (carrinho.length > 0) {
+        carrinho.forEach(item => {
+            total += item.preco;
+            const divItem = document.createElement('div');
+            divItem.classList.add('item-carrinho');
+            divItem.innerHTML = `
+                <h3>${item.nome}</h3>
+                <p>Preço: R$ ${item.preco}</p>
+            `;
+            carrinhoContainer.appendChild(divItem);
+        });
+        totalContainer.innerHTML = `Total: R$ ${total.toFixed(2)}`;
+    } else {
+        carrinhoContainer.innerHTML = '<p>Seu carrinho está vazio.</p>';
+        totalContainer.innerHTML = 'Total: R$ 0,00';
+    }
+}
+
+// Carrega os produtos do carrinho quando a página for carregada
+window.onload = adicionarAoCarrinho;
