a09d09434856086b93376bdafa5f594b609c7d07
Pedro_Daniel <pedrodaniel4878@gmail.com>
2025-03-21 20:31:14 -0300

    landingpage

--- JS/mobile-navbar.js
+++
@@ -1,6 +0,0 @@
-const menuMobile = document.getElementById('menu-mobile');
-const nav = document.querySelector('.nav-list');
-
-menuMobile.addEventListener('click', () => {
-    nav.classList.toggle('open');
-});
